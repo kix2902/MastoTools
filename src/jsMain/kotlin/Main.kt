8cd49ea9cfacaee414eaf0d7c3ba18a824768959
Ismael Reyes <ismael.kix2902@gmail.com>
2022-11-25 22:34:59 +0100

    Added routes in app for main and info screens

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,20 +1,37 @@
+import content.MainScreen
+import kotlinx.browser.window
+import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.css.em
 import org.jetbrains.compose.web.css.fontSize
 import org.jetbrains.compose.web.css.textAlign
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
+import org.w3c.dom.url.URLSearchParams
+import styles.AppStylesheet
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        Div(attrs = {
-            style {
-                textAlign("center")
-                fontSize(4.em)
+        Style(AppStylesheet)
+
+        val params = URLSearchParams(window.location.search)
+
+        val app = params.get("app")
+        when (app) {
+            "info" -> {
+                Div(attrs = {
+                    style {
+                        textAlign("center")
+                        fontSize(4.em)
+                    }
+                }) {
+                    Text("Mastodon Info")
+                }
             }
-        }) {
-            Text("It's working")
+
+            null -> MainScreen()
+
+            else -> window.location.href = "/"
         }
     }
 }
-
